a48b898717836ce8bfa53d9de68326e2b74b5170
Patrick Ambli <patrick.ambli@gmail.com>
2016-04-24 14:15:13 -0500

    Update hack.js

--- hack.js
+++ hack.js
@@ -6,6 +6,11 @@ var loadHack = function() {
   document.onmousewheel = (function(e) {
     this.scrol *= Math.pow(0.9, e.wheelDelta / -120 || e.detail / 2 || 0)
   }).bind(this);
-  document.onkeydown
+  document.onkeydown = (function(e){
+    if (e.keyCode == 16) setAcceleration(1);
+  }).bind(this);
+  document.onkeyup = (function(e){
+    if (e.keyCode == 16) setAcceleration(0);
+  }).bind(this);
 };
-loadHack()
+loadHack();
