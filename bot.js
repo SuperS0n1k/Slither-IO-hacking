2decf3e293f0a4c63f1223d63e35219c9728a1d7
Patrick Ambli <patrick.ambli@gmail.com>
2016-04-26 19:34:02 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 setInterval(()=>{
   xm = foods[0].xx - snake.xx;
   ym = foods[0].yy - snake.yy
-  ctx.beginPath();
-  ctx.arc(xm, ym, 20, 0, 2 * Math.PI, false);
-  ctx.fillStyle = 'green';
-  ctx.fill();
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = '#003300';
-  ctx.stroke();
+  this.ctx =  document.querySelector('canvas.nsi').getContext('2d');
+  this.ctx.beginPath();
+  this.ctx.arc(xm, ym, 20, 0, 2 * Math.PI, false);
+  this.ctx.fillStyle = 'green';
+  this.ctx.fill();
+  this.ctx.lineWidth = 5;
+  this.ctx.strokeStyle = '#003300';
+  this.ctx.stroke();
 },20)
 
