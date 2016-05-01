06ea6bada5cdd9ba705adcbfb018b41c904dd0d2
Patrick Ambli <patrick.ambli@gmail.com>
2016-05-01 15:19:51 -0500

    Create Cheats and stuff.meta.js

---
+++ Cheats-and-Stuff/Cheats and stuff.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Cheats and stuff
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  some fun Hacks mods and cheets
+// @author       Pcat
+// @match        @match http://slither.io/*
+// @downloadURL  https://github.com/Pcat0/Slither-IO-hacking/raw/master/Cheats-and-Stuff/Cheats%20and%20stuff.user.js
+// @updateUR     https://github.com/Pcat0/Slither-IO-hacking/raw/master/Cheats-and-Stuff/Cheats%20and%20stuff.user.js
+// ==/UserScript==
