3757df190f99a4844c01ab05aaa382815a48b8d4
Patrick Ambli <patrick.ambli@gmail.com>
2016-05-01 15:08:37 -0500

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,24 +1,26 @@
 // ==UserScript==
-// @name Slither Hscks
-// @namespace Cheats and stuff
-// @version 0.1
-// @description Some fun Hacks mods and cheets
-// @match http://slither.io/*
+// @name         Slither Hacks
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  some fun Hacks mods and cheets
+// @author       Pcat
+// @match        @match http://slither.io/*
+// @grant        none
 // ==/UserScript==
-var loadHack = function() {
-  this.scrol = .9;
-  setInterval(() => {
-    gsc = this.scrol
-  }, 1);
-  document.onmousewheel = (function(e) {
-    this.scrol *= Math.pow(0.9, e.wheelDelta / -120 || e.detail / 2 || 0)
-  }).bind(this);
-  document.onkeydown = (function(e){
-    if (e.keyCode == 16) setAcceleration(1);
-  }).bind(this);
-  document.onkeyup = (function(e){
-    if (e.keyCode == 16) setAcceleration(0);
-  }).bind(this);
-};
-loadHack();
-//setInterval(()=>{xm = foods[0].xx - snake.xx, ym = foods[0].yy - snake.yy},1)
+
+(function() {
+    'use strict';
+    this.scrol = .9;
+    setInterval(function(){
+        gsc = this.scrol;
+    }, 1);
+    document.onmousewheel = (function(e) {
+        this.scrol *= Math.pow(0.9, e.wheelDelta / -120 || e.detail / 2 || 0);
+    }).bind(this);
+    document.onkeydown = (function(e){
+        if (e.keyCode == 16) setAcceleration(1);
+    }).bind(this);
+    document.onkeyup = (function(e){
+        if (e.keyCode == 16) setAcceleration(0);
+    }).bind(this);
+})();
